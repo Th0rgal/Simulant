191b692a08a19f051fe174da997a0aaa892dbd9e
Thomas Marchand <me@thomas-marchand.com>
2021-05-08 11:07:49 +0200

    refactor game

--- src/controllers/game.cpp
+++ src/controllers/game.cpp
@@ -14,11 +14,8 @@ void Game::restart()
 {
     grid.clear();
     grid = Grid(3);
-
     delta.clear();
-
     view.clear();
-
     view.init_grid(grid);
     view.renderAll();
 }
@@ -51,20 +48,11 @@ void Game::start()
     }
 }
 
-Action move_ant_on_screen(Grid &grid, Ant *ant, Coordinates location)
-{
-    Action action;
-    action.type = ActionType::AntMove;
-    action.updated.push_back(ant->get_location());
-    action.updated.push_back(location);
-    action.colony = ant->get_colony();
-    return action;
-}
-
 void Game::loop(unsigned long delay, size_t current_block)
 {
     delta.clear();
 
+    // spawn new ants
     grid.map_colony([&](Colony *colony) {
         std::vector<Coordinates> new_ants = spawn_list[colony];
         std::vector<Coordinates> updated_new_ants;
@@ -78,75 +66,23 @@ void Game::loop(unsigned long delay, size_t current_block)
         spawn_list[colony] = updated_new_ants;
     });
 
+    // perform tour logic
     std::vector<Ant *> killed;
     std::vector<Ant *> in_fight;
     grid.map_ants([&](size_t i, Ant *ant) {
         if (std::find(killed.begin(), killed.end(), ant) == killed.end() && std::find(in_fight.begin(), in_fight.end(), ant) == in_fight.end())
-        {
-            Cell *next_cell = ant->find_move(grid, current_block);
-            if (next_cell != NULL)
-            {
-
-                // fight
-                if (next_cell->has_ant())
-                {
-                    // me move the attacker to the new case on the screen
-                    delta.push_back(move_ant_on_screen(grid, ant, next_cell->get_location()));
-
-                    if (flip_a_coin())
-                    {
-                        killed.push_back(next_cell->get_ant());
-                        ant->move(grid, next_cell);
-                    }
-                    else
-                    {
-                        killed.push_back(ant);
-                        in_fight.push_back(next_cell->get_ant());
-                    }
-                }
-
-                // let's take sugar
-                else if (!ant->has_sugar() && next_cell->has_sugar())
-                {
-                    ant->add_sugar();
-                    delta.push_back(move_ant_on_screen(grid, ant, ant->get_location()));
-                }
-
-                // let's deposit sugar
-                else if (next_cell->is_nest() && next_cell->get_nest() == ant->get_colony())
-                {
-                    if (ant->has_sugar())
-                    {
-                        grid.get_cell(ant->get_location())->add_sugar_pheromon();
-                        if (ant->deposit_sugar()) // if an ant spawned
-                            spawn_list[ant->get_colony()].push_back(ant->get_location());
-                    }
-                    delta.push_back(move_ant_on_screen(grid, ant, ant->get_location()));
-                }
-
-                // juste move
-                else
-                {
-                    if (ant->has_sugar())
-                        grid.get_cell(ant->get_location())->add_sugar_pheromon();
-                    delta.push_back(move_ant_on_screen(grid, ant, next_cell->get_location()));
-                    ant->move(grid, next_cell);
-                }
-            }
-            else
-                delta.push_back(move_ant_on_screen(grid, ant, ant->get_location()));
-        }
+            apply_ant_logic(ant, ant->find_move(grid, current_block), killed, in_fight);
         else
-            delta.push_back(move_ant_on_screen(grid, ant, ant->get_location()));
+            display_not_moving_ant(ant);
     });
 
+    // kill ants
     for (Ant *ant : killed)
     {
         if (ant == NULL)
             continue;
         Action action;
         action.type = ActionType::AntDeath;
-        std::cout << "killed:" << ant->get_location() << std::endl;
         action.updated.push_back(ant->get_location());
         // winner's colony
         action.colony = grid.get_cell(ant->get_location())->get_ant()->get_colony();
@@ -154,4 +90,75 @@ void Game::loop(unsigned long delay, size_t current_block)
         Colony *colony = ant->get_colony();
         colony->remove_ant(grid, colony->find_ant_index(ant));
     }
+}
+
+void Game::apply_ant_logic(Ant *ant, Cell *next_cell, std::vector<Ant *> &killed, std::vector<Ant *> &in_fight)
+{
+    if (next_cell == NULL)
+    {
+        display_not_moving_ant(ant);
+        return;
+    }
+
+    // fight
+    if (next_cell->has_ant())
+    {
+        // me move the attacker to the new case on the screen
+        move_ant_on_view(ant, next_cell->get_location());
+
+        if (flip_a_coin())
+        {
+            killed.push_back(next_cell->get_ant());
+            ant->move(grid, next_cell);
+        }
+        else
+        {
+            killed.push_back(ant);
+            in_fight.push_back(next_cell->get_ant());
+        }
+    }
+
+    // let's take sugar
+    else if (!ant->has_sugar() && next_cell->has_sugar())
+    {
+        ant->add_sugar();
+        move_ant_on_view(ant, ant->get_location());
+    }
+
+    // let's deposit sugar
+    else if (next_cell->is_nest() && next_cell->get_nest() == ant->get_colony())
+    {
+        if (ant->has_sugar())
+        {
+            grid.get_cell(ant->get_location())->add_sugar_pheromon();
+            if (ant->deposit_sugar()) // if an ant spawned
+                spawn_list[ant->get_colony()].push_back(ant->get_location());
+        }
+        move_ant_on_view(ant, ant->get_location());
+    }
+
+    // juste move
+    else
+    {
+        if (ant->has_sugar())
+            grid.get_cell(ant->get_location())->add_sugar_pheromon();
+
+        move_ant_on_view(ant, next_cell->get_location());
+        ant->move(grid, next_cell);
+    }
+}
+
+void Game::display_not_moving_ant(Ant *ant)
+{
+    move_ant_on_view(ant, ant->get_location());
+}
+
+void Game::move_ant_on_view(Ant *ant, Coordinates location)
+{
+    Action action;
+    action.type = ActionType::AntMove;
+    action.updated.push_back(ant->get_location());
+    action.updated.push_back(location);
+    action.colony = ant->get_colony();
+    delta.push_back(action);
 }
--- src/controllers/game.hpp
+++ src/controllers/game.hpp
@@ -16,4 +16,8 @@ private:
     std::vector<Action> delta;
     std::map<Colony *, std::vector<Coordinates>> spawn_list;
     void loop(unsigned long delay, size_t current_block);
+    void apply_ant_logic(Ant * ant, Cell *next_cell, std::vector<Ant*> &killed, std::vector<Ant*> &in_fight);
+
+    void display_not_moving_ant(Ant *ant);
+    void move_ant_on_view(Ant *ant, Coordinates location);
 };
