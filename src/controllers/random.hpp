16431eb200cc277dc58987a8761d9b68d3815ab6
Thomas Marchand <me@thomas-marchand.com>
2021-05-10 12:08:19 +0200

    add documentation to random.hpp

--- src/controllers/random.hpp
+++ src/controllers/random.hpp
@@ -2,6 +2,38 @@
 
 #include "stdlib.h"
 
+/**
+ * Return a random int between two boundaries (included)
+ * 
+ * @param int lower boundary
+ * @param int higher boundary
+ * 
+ * @return int random output
+ * 
+ * @confidence 5
+ * @author Thomas Marchand
+ **/
 int random_index(int lower, int higher);
+
+/**
+ * Return a random double between two boundaries (included)
+ * 
+ * @param double lower boundary
+ * @param double higher boundary
+ * 
+ * @return double random output
+ * 
+ * @confidence 5
+ * @author Thomas Marchand
+ **/
 double random_double(double lower, double higher);
+
+/**
+ * Return a random boolean (50% true, 50% false)
+ * 
+ * @return bool random output
+ * 
+ * @confidence 5
+ * @author Thomas Marchand
+ **/
 bool flip_a_coin();
