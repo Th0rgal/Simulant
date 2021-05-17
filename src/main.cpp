8cfed83c784c9cc7f7bc569f4475a00ae44b5c15
Thomas Marchand <me@thomas-marchand.com>
2021-05-17 16:46:34 +0200

    remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-#include "controllers/ant.hpp"
 #include "controllers/game.hpp"
-#include "view/view.hpp"
-#include <vector>
-// # include <SDL2/SDL.h>
-// # include <SDL2/SDL_timer.h>
 
 int main()
 {
-    Game game;
-    game.start();
-    //create_ants_array({Coordinates(1, -2)});
+    Game().start();
     return 0;
 }
