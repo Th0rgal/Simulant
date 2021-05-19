7ec9a6168bd08a40b66a69777c76bb9ea37c5448
Thomas Marchand <me@thomas-marchand.com>
2021-05-19 19:17:32 +0200

    introduce cli options

---
+++ src/controllers/cli.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class GameOptions {
+
+    GameOptions(int args_size, char *args[]);
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "controllers/game.hpp"
 
-int main()
+int main(int args_size, char *args[])
 {
     Game().start();
     return 0;
